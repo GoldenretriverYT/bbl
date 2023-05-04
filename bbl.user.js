8ed3c6c533b345486bae2588a6d7447b42d0b2c8
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-05-04 07:38:26 +0200

    Create bbl.user.js

---
+++ bbl.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         BetterBlockLands Reblocked
+// @namespace    http://tampermonkey.net/
+// @version      4.0
+// @description  Better Block Lands 4.0 is for the new Block Lands
+// @author       GoldenretriverYT
+// @match        *://*.block-lands.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=block-lands.com
+// @grant        none
+// @updateURL   
+// ==/UserScript==
